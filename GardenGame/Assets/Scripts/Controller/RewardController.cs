319b2bf79bc28d26a8e7617cc5b859174e0b0d7e
dato-svg <139239441+dato-svg@users.noreply.github.com>
2024-02-21 20:56:49 +0400

    ad Reward and Localization
    ad Reward and Localization

--- GardenGame/Assets/Scripts/Controller/RewardController.cs
+++ GardenGame/Assets/Scripts/Controller/RewardController.cs
@@ -1,18 +1,25 @@
 using System;
-using System.Collections;
 using System.Threading.Tasks;
+using Resources;
+using Saver;
 using UnityEngine;
+using YG;
 
 namespace Controller
 {
     public class RewardController : MonoBehaviour
     {
         [SerializeField] private GameObject _target;
+        [SerializeField] private GameObject _language;
+        [SerializeField] private SaveManager _saveManager;
         private Animator _animator;
         private float _timeDelay= 150f;
+        private int _moneyCount = 300;
+        private bool isActive = false;
         
         private void Start()
-        {    
+        {
+            _saveManager = FindObjectOfType<SaveManager>();
             _target.SetActive(false);
             _animator = GetComponent<Animator>();
             EnableObject();
@@ -35,8 +42,33 @@ namespace Controller
             }
             
         }
-        
 
+
+        public void GiveReward()
+        {
+            ResourcesData.MoneyCount += _moneyCount;
+            _saveManager.SaveAll();
+        }
+
+        public void StartReward()
+        {
+            YandexGame.RewVideoShow(0);
+        }
+
+        public void LanguageChange()
+        {
+            if (!isActive)
+            {
+                _language.SetActive(true);
+                isActive = true;
+            }
+            else
+            {
+                _language.SetActive(false);
+                isActive = false;
+            }
+            
+        }
         
     }
 }
